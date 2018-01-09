9bf3b3bbbd733e5fa857a795d42cc92796483d1d
Ben Grant <Benjah88@gmail.com>
2018-01-09 11:06:07 -0800

    ok

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ mongoose.connect("mongodb://localhost/reactdb", {
 
 }
 
-app.post("/save", function(req, res) {
+app.post("/app/save", function(req, res) {
 
   console.log(req.body);
 
@@ -70,7 +70,7 @@ app.post("/save", function(req, res) {
 
     });
 
-app.get("/articles", function(req, res) {
+app.get("/app/articles", function(req, res) {
 
 db.Article
     .find({})
@@ -84,7 +84,7 @@ db.Article
     })
 });
 
-app.post('/note/:id', function(req, res){
+app.post('/app/note/:id', function(req, res){
 
 console.log(req.body);
 
@@ -113,7 +113,7 @@ console.log(req.body);
     });
 });
 
-app.post("/delnote/:id", function(req, res) {
+app.post("/app/delnote/:id", function(req, res) {
 
   console.log(req.params.id)
 
@@ -137,7 +137,7 @@ app.post("/delnote/:id", function(req, res) {
     });
 
 
-app.post("/delete", function(req, res) {
+app.post("/app/delete", function(req, res) {
 
   console.log(req.body.info.title);
 
